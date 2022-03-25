ce16ebd3e3b24bac2ce6be59e32b64c5f5c91329
Glen <mistergjones@yahoo.com.au>
2022-03-25 12:21:58 +1100

    Finalised Logic

--- Grid.js
+++ Grid.js
@@ -3,6 +3,9 @@ const CELL_SIZE = 20;
 const CELL_GAP = 2;
 
 export default class Grid {
+    // introduce a private field for this class
+    #cells;
+
     // establish the consructor for a grid element
     constructor(gridElement) {
         // now create the grid and set/assign CSS properties
@@ -10,11 +13,112 @@ export default class Grid {
         gridElement.style.setProperty("--cell-size", `${CELL_SIZE}vmin`);
         gridElement.style.setProperty("--cell-gap", `${CELL_GAP}vmin`);
 
-        // create an individual cell
-        createCellElements(gridElement);
+        // create all the required cells and give them x & y positions also
+        this.#cells = createCellElements(gridElement).map(
+            (cellElement, index) => {
+                return new Cell(
+                    cellElement,
+                    index % GRID_SIZE,
+                    Math.floor(index / GRID_SIZE)
+                );
+            }
+        );
+    }
+
+    get cells() {
+        return this.#cells;
+    }
+
+    get cellsByColumn() {
+        return this.#cells.reduce((cellGrid, cell) => {
+            cellGrid[cell.x] = cellGrid[cell.x] || [];
+            cellGrid[cell.x][cell.y] = cell;
+            return cellGrid;
+        }, []);
+    }
+
+    get cellsByRow() {
+        return this.#cells.reduce((cellGrid, cell) => {
+            cellGrid[cell.y] = cellGrid[cell.y] || [];
+            cellGrid[cell.y][cell.x] = cell;
+            return cellGrid;
+        }, []);
+    }
+
+    get #emptyCells() {
+        return this.#cells.filter((cell) => cell.tile == null);
+    }
+
+    // generate a random cell
+    randomEmptyCell() {
+        const randomIndex = Math.floor(Math.random() * this.#emptyCells.length);
+        return this.#emptyCells[randomIndex];
     }
 }
 
+class Cell {
+    // make the members private to this class
+    #cellElement;
+    #x;
+    #y;
+    #tile;
+    #mergeTile;
+    constructor(cellElement, x, y) {
+        this.#cellElement = cellElement;
+        this.#x = x;
+        this.#y = y;
+    }
+
+    get x() {
+        return this.#x;
+    }
+    get y() {
+        return this.#y;
+    }
+
+    get tile() {
+        return this.#tile;
+    }
+
+    set tile(value) {
+        this.#tile = value;
+        if (value == null) return;
+
+        // move fromn old position to new position
+        this.#tile.x = this.#x;
+        this.#tile.y = this.#y;
+    }
+
+    get mergeTile() {
+        return this.#mergeTile;
+    }
+
+    set mergeTile(value) {
+        this.#mergeTile = value;
+        if (value == null) return;
+        this.#mergeTile.x = this.#x;
+        this.#mergeTile.y = this.#y;
+    }
+
+    canAccept(tile) {
+        return (
+            this.tile == null ||
+            (this.mergeTile == null && this.tile.value === tile.value)
+        );
+    }
+
+    mergeTiles() {
+        if (this.tile == null || this.mergeTile == null) return;
+
+        this.tile.value = this.tile.value + this.mergeTile.value;
+
+        this.mergeTile.remove();
+
+        this.mergeTile = null;
+    }
+}
+
+// Create each cell element based on the grid sizes. e.g. 4x4, 5X5
 function createCellElements(gridElement) {
     const cells = [];
     for (let i = 0; i < GRID_SIZE * GRID_SIZE; i++) {
--- script.js
+++ script.js
@@ -1,6 +1,155 @@
 import Grid from "./Grid.js";
+import Tile from "./Tile.js";
 
 const gameBoard = document.getElementById("gameBoard");
 
 // create a grid instance
 const grid = new Grid(gameBoard);
+
+grid.randomEmptyCell().tile = new Tile(gameBoard);
+grid.randomEmptyCell().tile = new Tile(gameBoard);
+setupInput();
+
+function setupInput() {
+    window.addEventListener("keydown", handleInput, { once: true });
+}
+
+async function handleInput(e) {
+    console.log(e.key);
+    switch (e.key) {
+        case "ArrowUp":
+            if (!canMoveUp()) {
+                setupInput();
+                return;
+            }
+            await moveUp();
+            break;
+        case "ArrowDown":
+            if (!canMoveDown()) {
+                setupInput();
+                return;
+            }
+            await moveDown();
+            break;
+        case "ArrowRight":
+            if (!canMoveRight()) {
+                setupInput();
+                return;
+            }
+            await moveRight();
+            break;
+        case "ArrowLeft":
+            if (!canMoveLeft()) {
+                setupInput();
+                return;
+            }
+            await moveLeft();
+            break;
+        default:
+            setupInput();
+            return;
+    }
+
+    // merge the tiles
+    grid.cells.forEach((cell) => cell.mergeTiles());
+
+    const newTile = new Tile(gameBoard);
+    grid.randomEmptyCell().tile = newTile;
+
+    setupInput();
+
+    if (!canMoveUp() && !canMoveDown() && !canMoveLeft() && !canMoveRight()) {
+        newTile.waitForTransition(true).then(() => {
+            alert("You Lose");
+        });
+        return;
+    }
+    setupInput();
+}
+
+function moveUp() {
+    return slideTiles(grid.cellsByColumn);
+}
+
+function moveDown() {
+    return slideTiles(
+        grid.cellsByColumn.map((column) => [...column].reverse())
+    );
+}
+
+function moveLeft() {
+    return slideTiles(grid.cellsByRow);
+}
+
+function moveRight() {
+    return slideTiles(grid.cellsByRow.map((row) => [...row].reverse()));
+}
+
+function slideTiles(cells) {
+    return Promise.all(
+        cells.flatMap((group) => {
+            const promises = [];
+            // loop through the coloumn
+            for (let i = 1; i < group.length; i++) {
+                const cell = group[i];
+                // if the cell is null....simply continue
+                if (cell.tile == null) continue;
+                let lastValidCell;
+                // check if I can move updwards??? Check cell above
+                for (let j = i - 1; j >= 0; j--) {
+                    const moveToCell = group[j];
+                    // if you can't move up at lest 1 tile...simply break. No need ot check anything else
+                    if (!moveToCell.canAccept(cell.tile)) break;
+                    lastValidCell = moveToCell;
+                }
+                // can we move this tile? If we can, run this code
+                if (lastValidCell != null) {
+                    // if we have a tile that can move....wait for transitio nto finish
+                    promises.push(cell.tile.waitForTransition());
+                    // do we have a tile in which we are moving into ???
+                    if (lastValidCell.tile != null) {
+                        // if so, set the merge tile
+                        lastValidCell.mergeTile = cell.tile;
+                    } else {
+                        // if no tile, set it to the current tile
+                        lastValidCell.tile = cell.tile;
+                    }
+                    cell.tile = null;
+                }
+            }
+            return promises;
+        })
+    );
+}
+
+function canMoveUp() {
+    return canMove(grid.cellsByColumn);
+}
+
+function canMoveDown() {
+    return canMove(grid.cellsByColumn.map((column) => [...column].reverse()));
+}
+
+function canMoveLeft() {
+    return canMove(grid.cellsByRow);
+}
+
+function canMoveRight() {
+    return canMove(grid.cellsByRow.map((row) => [...row].reverse()));
+}
+
+function canMove(cells) {
+    return cells.some((group) => {
+        return group.some((cell, index) => {
+            // check if we cadn move at all
+            if (index === 0) return false;
+
+            // cant move an empty c ell
+            if (cell.tile == null) return false;
+
+            // check the cell above...if okay..we can move
+            const moveToCell = group[index - 1];
+            return moveToCell.canAccept(cell.tile);
+        });
+    });
+}
